5bd64be5423da3b4d63552a1bd7cad774500674f
Aadil Ali <61555486+aadilaliofficial@users.noreply.github.com>
2025-08-13 14:47:00 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -11,7 +11,7 @@ export default function Register() {
     e.preventDefault();
 
     try {
-      await axios.post("https://manager-dashboard-mern.vercel.app/register", {
+      await axios.post("http://localhost:5000/api/auth/register", {
         username,
         password
       });
